3e2b072c1d0f15c6fa0b7bc1b4ea0d39e1f3fd05
Brett Kaplan <BrettCKaplan@gmail.com>
2026-03-23 22:46:24 -0400

    fonts dir

--- renderer.py
+++ renderer.py
@@ -8,7 +8,7 @@ import data
 small_font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 16)
 medium_font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 20)
 large_font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 24)
-icon_font = ImageFont.truetype("./meteocons.ttf", 48)
+icon_font = ImageFont.truetype("./fonts/meteocons.ttf", 48)
 ICON_MAP = {
     "01d": "B",
     "01n": "C",
