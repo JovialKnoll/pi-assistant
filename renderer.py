ee5b000bdc1d5be44f45f597f4f2166abe13c011
Brett Kaplan <BrettCKaplan@gmail.com>
2026-03-25 09:51:27 -0400

    Update renderer.py

--- renderer.py
+++ renderer.py
@@ -52,9 +52,8 @@ medium_font_example_bbox = medium_font.getbbox(example_text)
 medium_font_example_height = medium_font_example_bbox[3] - medium_font_example_bbox[1]
 large_font_example_bbox = large_font.getbbox(example_text)
 large_font_example_height = large_font_example_bbox[3] - large_font_example_bbox[1]
-
-full_temp_c_width = large_font.getbbox("000°C")[2]
-full_temp_both_width = large_font.getbbox("100°F000°C")[2]
+full_temp_c_width = large_font.getbbox("-00°C")[2]
+full_temp_both_width = large_font.getbbox("100°F-00°C")[2]
 
 
 def _get_celsius(kelvin):
