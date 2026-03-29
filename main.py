7124b1e0a8c470293edea7f501035327f67b7697
Brett Kaplan <BrettCKaplan@gmail.com>
2026-03-29 15:11:00 -0400

    not needed

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ import busio
 import board
 import digitalio
 from adafruit_debouncer import Debouncer
-#from adafruit_epd.epd import Adafruit_EPD
 from adafruit_epd.ssd1680 import Adafruit_SSD1680
 from PIL import ImageChops
 
