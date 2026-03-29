36bfc419346b97a272eb91c7ee9866ce6ee20f2c
Brett Kaplan <BrettCKaplan@gmail.com>
2026-03-29 15:49:53 -0400

    no margin needed

--- renderer.py
+++ renderer.py
@@ -30,7 +30,7 @@ ICON_MAP = {
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 MARGIN_X = 0
-MARGIN_Y = 1
+MARGIN_Y = 0
 SPACING = 1
 small_font = ImageFont.truetype(
     Path(__file__).parent / 'fonts' / 'dejavu' / 'DejaVuSans-Bold.ttf',
@@ -99,7 +99,7 @@ def get_weather_display(label, date, weather):
     icon_y_offset = icon_bbox[1]
     icon_height = icon_bbox[3] - icon_y_offset
     icon_width = icon_bbox[2]
-    icon_top = main_top - SPACING - icon_height
+    icon_top = main_top - SPACING - SPACING - icon_height
     icon_right = MARGIN_X + SPACING + icon_width
     draw.text(
         (icon_right - icon_width, icon_top - icon_y_offset),
@@ -205,7 +205,7 @@ def get_weather_display(label, date, weather):
 
     wind_deg = weather["wind"]["deg"]
     wind_radians = math.radians((270 - wind_deg) % 360)
-    midway = (config.HEIGHT - small_font_example_height - wind_speed_bottom) // 2
+    midway = (config.HEIGHT - small_font_example_height - SPACING - wind_speed_bottom) // 2
     center = (config.WIDTH - midway - SPACING, config.HEIGHT - small_font_example_height - SPACING - midway)
     radius = config.HEIGHT - MARGIN_Y - small_font_example_height - SPACING - center[1]
     draw.circle(center, radius, outline=BLACK)
