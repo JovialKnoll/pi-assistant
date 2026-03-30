bb6617ada1fd96f8efb369e70bb639fa49443974
Brett Kaplan <BrettCKaplan@gmail.com>
2026-03-30 00:20:50 -0400

    move markers inward

--- renderer.py
+++ renderer.py
@@ -210,10 +210,10 @@ def get_weather_display(label, date, weather):
     radius = config.HEIGHT - MARGIN_Y - small_font_example_height - SPACING - center[1]
     draw.circle(center, radius, outline=BLACK, width=2)
     draw.point((
-        (center[0], center[1] - radius + 1),
-        (center[0], center[1] + radius - 1),
-        (center[0] - radius + 1, center[1]),
-        (center[0] + radius - 1, center[1]),
+        (center[0], center[1] - radius + 2),
+        (center[0], center[1] + radius - 2),
+        (center[0] - radius + 2, center[1]),
+        (center[0] + radius - 2, center[1]),
         ), fill=BLACK)
     draw.line((
         center,
