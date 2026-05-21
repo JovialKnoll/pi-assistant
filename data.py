11a32dbc1d988974850fc10e0b1117b423cba986
Brett Kaplan <BrettCKaplan@gmail.com>
2026-05-20 20:55:43 -0400

    logging

--- data.py
+++ data.py
@@ -1,7 +1,13 @@
 import json
+import logging
 import urllib.request
 
 import config
+
+
+ATTEMPT_MAX = 3
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename='data.log', encoding='utf-8', format='%(asctime)s %(message)s', level=logging.DEBUG)
 
 
 def get_weather(lat, long):
@@ -10,13 +16,29 @@ def get_weather(lat, long):
         lat,
         long,
     )
+    for i in range(ATTEMPT_MAX):
+        weather = get_weather_internal(url)
+        if weather is not None:
+            return weather
+    logger.error(f'Failed to get weather {ATTEMPT_MAX} times')
+    return None
+
+
+def get_weather_internal(url):
     request = urllib.request.Request(url)
-    response = urllib.request.urlopen(request)
-    if response.getcode() != 200:
+    try:
+        response = urllib.request.urlopen(request)
+    except Exception as e:
+        logger.warning(f'Exception on urlopen: {e}')
+        return None
+    code = response.getcode()
+    if code != 200:
+        logger.warning(f'Response code was {code}')
         return None
     content = response.read()
     weather = json.loads(content)
     if weather['cod'] == '404':
+        logger.warning('Weather returned 404')
         return None
     #with open('test.json', 'w') as f:
     #    json.dump(weather, f, indent=4)
